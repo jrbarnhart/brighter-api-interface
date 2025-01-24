c4231d74eca248598130c13ce3042b7e4176c261
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-24 15:38:58 -0500

    Add simple schema for create region input

--- src/features/regions/schemas/create-reagion.schema.ts
+++ src/features/regions/schemas/create-reagion.schema.ts
@@ -2,6 +2,6 @@ import { paths } from "@/types/api";
 import { ZodSchemaFromType } from "@/types/zod";
 import { z } from "zod";
 
-export const CreateRegionZod = z.object({
+export const CreateRegionSchema = z.object({
   name: z.string(),
 } satisfies ZodSchemaFromType<paths["/regions"]["post"]["requestBody"]["content"]["application/json"]>);
