64071d3b166e8bcf0f3281e21b395b3fa473d2aa
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-24 18:06:43 -0500

    Move query to index component

--- src/features/regions/components/RegionsIndex.tsx
+++ src/features/regions/components/RegionsIndex.tsx
@@ -1,4 +1,31 @@
+import { components } from "@/types/api";
+import { useQuery } from "@tanstack/react-query";
+
 export default function RegionsIndex() {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  const { isLoading, isSuccess, error, data } = useQuery({
+    queryKey: ["allRegions"],
+    queryFn: (): Promise<{ data: components["schemas"]["RegionEntity"][] }> =>
+      fetch(`${import.meta.env.VITE_API_URL}/regions`, {
+        headers: new Headers(),
+      }).then((res) => res.json()),
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  if (!isSuccess) {
+    return (
+      <div>
+        <p>There was a problem fetching the Regions data.</p>
+      </div>
+    );
+  }
+
+  const { data: payload } = data;
   return (
     <div>
       <p>Total: 1</p>
--- src/features/regions/pages/Regions.tsx
+++ src/features/regions/pages/Regions.tsx
@@ -1,34 +1,7 @@
 import FeatureHeader from "@/components/featureHeader/FeatureHeader";
-import { components } from "@/types/api";
-import { useQuery } from "@tanstack/react-query";
 import { Outlet } from "react-router";
 
 export default function Regions() {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
-  const { isLoading, isSuccess, error, data } = useQuery({
-    queryKey: ["allRegions"],
-    queryFn: (): Promise<{ data: components["schemas"]["RegionEntity"][] }> =>
-      fetch(`${import.meta.env.VITE_API_URL}/regions`, {
-        headers: new Headers(),
-      }).then((res) => res.json()),
-  });
-
-  if (isLoading) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-
-  if (!isSuccess) {
-    return (
-      <div>
-        <p>There was a problem fetching the Regions data.</p>
-      </div>
-    );
-  }
-
-  const { data: payload } = data;
-
   return (
     <>
       <FeatureHeader label="Regions" urlName="regions" />
