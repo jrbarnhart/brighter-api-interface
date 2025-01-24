4a880057120db55bccc5675cb8fa58d00c0abfc3
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-24 15:39:16 -0500

    Add simple form for adding region

--- src/features/regions/components/RegionsForm.tsx
+++ src/features/regions/components/RegionsForm.tsx
@@ -1,10 +1,51 @@
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { CreateRegionSchema } from "../schemas/create-reagion.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 export default function RegionsForm() {
+  const form = useForm<z.infer<typeof CreateRegionSchema>>({
+    resolver: zodResolver(CreateRegionSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof CreateRegionSchema>) => {
+    // React query goes here
+    console.log(values);
+  };
+
   return (
-    <div>
-      <p>Regions Form</p>
-      <Button>Submit</Button>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Region Name" {...field} />
+              </FormControl>
+              <FormDescription>The name of the region.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
