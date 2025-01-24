fdf37b79bf7a2d89bfd0afc26f21812ff4398701
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-23 23:10:31 -0500

    Add simple query using new types

--- src/features/regions/pages/Regions.tsx
+++ src/features/regions/pages/Regions.tsx
@@ -1,23 +1,44 @@
-// import { useQuery } from "@tanstack/react-query";
+import { components } from "@/types/api";
+import { useQuery } from "@tanstack/react-query";
 import { Outlet } from "react-router";
 
 export default function Regions() {
-  // const query = useQuery({
-  //   queryKey: ["regions"],
-  //   queryFn: async () => {
-  //     const response = await fetch(`${process.env.API_URL ?? ""}/regions`);
-  //     if (!response.ok) {
-  //       throw new Error("Network response was not ok");
-  //     }
-  //     return response.json();
-  //   },
-  // });
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  const { isLoading, isSuccess, error, data } = useQuery({
+    queryKey: ["allRegions"],
+    queryFn: (): Promise<{ data: components["schemas"]["RegionEntity"][] }> =>
+      fetch(`${import.meta.env.VITE_API_URL}/regions`, {
+        headers: new Headers(),
+      }).then((res) => res.json()),
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  if (!isSuccess) {
+    return (
+      <div>
+        <p>There was a problem fetching the Regions data.</p>
+      </div>
+    );
+  }
+
+  const { data: payload } = data;
 
   return (
     <div>
       <h1>Regions:</h1>
-      <p>Count: 10</p>
-      <p>Total Rooms: 100</p>
+      <p>{`Count: ${payload.length.toString() || "Not found"}`}</p>
+      <p>
+        Total Rooms:{" "}
+        {payload.reduce(
+          (roomCount, currentRegion) => roomCount + currentRegion.rooms.length,
+          0
+        )}
+      </p>
       <Outlet />
     </div>
   );
