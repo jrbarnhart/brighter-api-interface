8ef9893fb83da36ab232c04efe9a4a6094ce59fd
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-24 16:32:04 -0500

    Require at least one character

--- src/features/regions/schemas/create-reagion.schema.ts
+++ src/features/regions/schemas/create-reagion.schema.ts
@@ -3,5 +3,5 @@ import { ZodSchemaFromType } from "@/types/zod";
 import { z } from "zod";
 
 export const CreateRegionSchema = z.object({
-  name: z.string(),
+  name: z.string().trim().nonempty(),
 } satisfies ZodSchemaFromType<paths["/regions"]["post"]["requestBody"]["content"]["application/json"]>);
