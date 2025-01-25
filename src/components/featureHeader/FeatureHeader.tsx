9a7e57e85d99fb06b836fb7b9d189ffb394ce5b3
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-25 14:50:25 -0500

    Adjust positioning of header to fixed

--- src/components/featureHeader/FeatureHeader.tsx
+++ src/components/featureHeader/FeatureHeader.tsx
@@ -9,21 +9,23 @@ export default function FeatureHeader({
   urlName: string;
 }) {
   return (
-    <div className="flex flex-col h-8">
-      <div className="flex gap-2 items-start h-full">
-        <Button variant={"link"} className="pl-0 pr-2" asChild>
-          <Link to={"/regions"}>
-            <h1 className="text-3xl">{label}</h1>
-          </Link>
-        </Button>
-        <div className="w-1 h-full bg-secondary rounded-xl border-border" />
-        <Button
-          variant={"link"}
-          className="pl-1 text-accent text-xl items-start"
-          asChild
-        >
-          <Link to={`/${urlName}/create`}>Create</Link>
-        </Button>
+    <div className="py-4 -mt-4 bg-background fixed z-10 w-full">
+      <div className="flex flex-col h-8">
+        <div className="flex gap-2 items-start h-full">
+          <Button variant={"link"} className="pl-0 pr-2" asChild>
+            <Link to={"/regions"}>
+              <h1 className="text-3xl">{label}</h1>
+            </Link>
+          </Button>
+          <div className="w-1 h-full bg-secondary rounded-xl border-border" />
+          <Button
+            variant={"link"}
+            className="pl-1 text-accent text-xl items-start"
+            asChild
+          >
+            <Link to={`/${urlName}/create`}>Create</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
