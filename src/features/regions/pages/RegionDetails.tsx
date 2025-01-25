388155b8c087c67fe42f4e9041207ddfcfae4f1a
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-25 13:52:42 -0500

    Add details component that gets a region by id

---
+++ src/features/regions/pages/RegionDetails.tsx
@@ -0,0 +1,42 @@
+import FeatureHeader from "@/components/featureHeader/FeatureHeader";
+import { paths } from "@/types/api";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
+
+export default function RegionDetails() {
+  const { id } = useParams();
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  const { isLoading, isSuccess, error, data } = useQuery({
+    queryKey: ["allRegions"],
+    queryFn: (): Promise<{
+      data: paths["/regions/{id}"]["get"]["responses"]["200"]["content"]["application/json"];
+    }> =>
+      fetch(`${import.meta.env.VITE_API_URL}/regions/${id ?? "0"}`, {
+        headers: new Headers(),
+      }).then((res) => res.json()),
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  if (!isSuccess) {
+    return (
+      <div>
+        <p>There was a problem fetching the Regions data.</p>
+      </div>
+    );
+  }
+
+  const { data: payload } = data;
+
+  return (
+    <>
+      <FeatureHeader label="Regions" urlName="regions" />
+      <p>{payload.name}</p>
+    </>
+  );
+}
