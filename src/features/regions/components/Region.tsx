5e30fb90c9c680a1de56fca4be99907625ecda82
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-28 15:51:35 -0500

    Show update form when update btn click

--- src/features/regions/components/Region.tsx
+++ src/features/regions/components/Region.tsx
@@ -2,9 +2,13 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { paths } from "@/types/api";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { Link, useParams } from "react-router";
+import UpdateRegionForm from "./UpdateRegionForm";
 
 export default function Region() {
+  const [isUpdating, setIsUpdating] = useState(false);
+
   const { id } = useParams();
 
   const headers = new Headers();
@@ -37,82 +41,106 @@ export default function Region() {
   return (
     <ScrollArea className="h-full">
       <div className="space-y-4">
-        <h2 className="text-2xl">{region.name}</h2>
-        <p className="text-xl">Id: {region.id}</p>
-        <div className="flex flex-col gap-2">
-          <p className="text-xl">Rooms:</p>
-          <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
-            <div className="flex flex-col items-start">
-              {region.rooms.map((room) => (
-                <Button
-                  key={room.id}
-                  variant={"link"}
-                  className="text-base p-0 underline truncate"
-                  asChild
-                >
-                  <Link to={`/rooms/${room.id.toString()}`}>{room.name}</Link>
-                </Button>
-              ))}
-            </div>
-          </ScrollArea>
+        <div className="flex h-8 items-start gap-3">
+          <h2 className="text-2xl">{region.name}</h2>
+          <div className="w-1 h-full bg-secondary rounded-xl border-border" />
+          <Button
+            variant={"link"}
+            onClick={() => {
+              setIsUpdating((prev) => !prev);
+            }}
+            className="text-yellow-500 text-lg p-0"
+          >
+            {isUpdating ? "Cancel" : "Update"}
+          </Button>
+          <Button variant={"link"} className="text-destructive text-lg p-0">
+            Delete
+          </Button>
         </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-xl">Combat Skills:</p>
-          <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
-            <div className="flex flex-col items-start">
-              {region.combatSkills.map((skill) => (
-                <Button
-                  key={skill.id}
-                  variant={"link"}
-                  className="text-base p-0 underline truncate"
-                  asChild
-                >
-                  <Link to={`/skills/combat/${skill.id.toString()}`}>
-                    {skill.name}
-                  </Link>
-                </Button>
-              ))}
+        {isUpdating ? (
+          <UpdateRegionForm />
+        ) : (
+          <>
+            {" "}
+            <p className="text-xl">Id: {region.id}</p>
+            <div className="flex flex-col gap-2">
+              <p className="text-xl">Rooms:</p>
+              <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
+                <div className="flex flex-col items-start">
+                  {region.rooms.map((room) => (
+                    <Button
+                      key={room.id}
+                      variant={"link"}
+                      className="text-base p-0 underline truncate"
+                      asChild
+                    >
+                      <Link to={`/rooms/${room.id.toString()}`}>
+                        {room.name}
+                      </Link>
+                    </Button>
+                  ))}
+                </div>
+              </ScrollArea>
             </div>
-          </ScrollArea>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-xl">Gathering Skills:</p>
-          <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
-            <div className="flex flex-col items-start">
-              {region.gatheringSkills.map((skill) => (
-                <Button
-                  key={skill.id}
-                  variant={"link"}
-                  className="text-base p-0 underline truncate"
-                  asChild
-                >
-                  <Link to={`/skills/gathering/${skill.id.toString()}`}>
-                    {skill.name}
-                  </Link>
-                </Button>
-              ))}
+            <div className="flex flex-col gap-2">
+              <p className="text-xl">Combat Skills:</p>
+              <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
+                <div className="flex flex-col items-start">
+                  {region.combatSkills.map((skill) => (
+                    <Button
+                      key={skill.id}
+                      variant={"link"}
+                      className="text-base p-0 underline truncate"
+                      asChild
+                    >
+                      <Link to={`/skills/combat/${skill.id.toString()}`}>
+                        {skill.name}
+                      </Link>
+                    </Button>
+                  ))}
+                </div>
+              </ScrollArea>
             </div>
-          </ScrollArea>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-xl">Crafting Skills:</p>
-          <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
-            <div className="flex flex-col items-start">
-              {region.craftingSkills.map((skill) => (
-                <Button
-                  key={skill.id}
-                  variant={"link"}
-                  className="text-base p-0 underline truncate"
-                  asChild
-                >
-                  <Link to={`/skills/crafting/${skill.id.toString()}`}>
-                    {skill.name}
-                  </Link>
-                </Button>
-              ))}
+            <div className="flex flex-col gap-2">
+              <p className="text-xl">Gathering Skills:</p>
+              <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
+                <div className="flex flex-col items-start">
+                  {region.gatheringSkills.map((skill) => (
+                    <Button
+                      key={skill.id}
+                      variant={"link"}
+                      className="text-base p-0 underline truncate"
+                      asChild
+                    >
+                      <Link to={`/skills/gathering/${skill.id.toString()}`}>
+                        {skill.name}
+                      </Link>
+                    </Button>
+                  ))}
+                </div>
+              </ScrollArea>
             </div>
-          </ScrollArea>
-        </div>
+            <div className="flex flex-col gap-2">
+              <p className="text-xl">Crafting Skills:</p>
+              <ScrollArea className="w-fit max-w-96 min-w-44 h-44 bg-secondary shadow-inner shadow-accent rounded-xl px-3">
+                <div className="flex flex-col items-start">
+                  {region.craftingSkills.map((skill) => (
+                    <Button
+                      key={skill.id}
+                      variant={"link"}
+                      className="text-base p-0 underline truncate"
+                      asChild
+                    >
+                      <Link to={`/skills/crafting/${skill.id.toString()}`}>
+                        {skill.name}
+                      </Link>
+                    </Button>
+                  ))}
+                </div>
+              </ScrollArea>
+            </div>{" "}
+          </>
+        )}
       </div>
     </ScrollArea>
   );
