fbbec063d1b02056e71cd1f357451a31e7d70d40
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-28 20:30:21 -0500

    Add component for rendering region details content

---
+++ src/features/regions/components/RegionDetailsContents.tsx
@@ -0,0 +1,28 @@
+import ScrollList from "@/components/scrollList/ScrollList";
+import { components } from "@/types/api";
+
+export default function RegionDetailsContents(
+  region: components["schemas"]["RegionEntity"]
+) {
+  return (
+    <>
+      <p className="text-xl">Id: {region.id}</p>
+      <ScrollList basePath="rooms" title="Rooms" items={region.rooms} />
+      <ScrollList
+        basePath="skills/combat"
+        title="Combat Skills"
+        items={region.combatSkills}
+      />
+      <ScrollList
+        basePath="skills/gathering"
+        title="Gathering Skills"
+        items={region.gatheringSkills}
+      />
+      <ScrollList
+        basePath="skills/crafting"
+        title="Crafting Skills"
+        items={region.craftingSkills}
+      />
+    </>
+  );
+}
