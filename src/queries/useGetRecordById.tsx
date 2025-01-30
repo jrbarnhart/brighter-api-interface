6e997cfec19a827dc94fe434f86830f19f32248b
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-30 16:13:44 -0500

    Use resource name variable instead of hard coded regions

--- src/queries/useGetRecordById.tsx
+++ src/queries/useGetRecordById.tsx
@@ -3,12 +3,14 @@ import { useQuery } from "@tanstack/react-query";
 export default function useGetRecordById<T>({
   id,
   basePath,
+  recordName,
 }: {
   id: number | string;
   basePath: string;
+  recordName: string;
 }) {
   return useQuery({
-    queryKey: ["region-by-id"],
+    queryKey: [`${recordName}-by-id`],
     queryFn: (): Promise<T> =>
       fetch(`${import.meta.env.VITE_API_URL}${basePath}/${id.toString()}`, {
         headers: { "Content-type": "application/json" },
