c107aabbdfb8675f8d722d3f8d84169234ecff1c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-30 19:20:24 -0500

    Add links to the rest of the features

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -32,14 +32,14 @@ export default function Nav() {
               <Link to={"/skills"}>Skills</Link>
             </Button>
             <CollapsibleTrigger asChild>
-              <Button variant="ghost" size="sm">
+              <Button variant="ghost" size="sm" className="px-2">
                 <ChevronsUpDown className="h-4 w-4" />
                 <span className="sr-only">Toggle</span>
               </Button>
             </CollapsibleTrigger>
           </div>
           <CollapsibleContent>
-            <ul className="list-disc list-inside">
+            <ul className="list-['-_'] list-inside">
               <li>
                 <Button variant={"link"} className="text-base pl-0">
                   <Link to={"/skills/combat"}>Combat</Link>
@@ -72,6 +72,185 @@ export default function Nav() {
                   <Link to={"/skills/crafting/requirements"}>
                     Crafting Reqs
                   </Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/skills/crafting/recipes"}>Craft Recipes</Link>
+                </Button>
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+
+        <Button variant={"link"} className="text-xl pl-0">
+          <Link to={"/items"}>Items</Link>
+        </Button>
+
+        <Collapsible className="w-full">
+          <div className="flex justify-between items-center w-full">
+            <Button variant={"link"} className="text-xl px-0">
+              <Link to={"/items/resources"}>Resources</Link>
+            </Button>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <ChevronsUpDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="list-['-_'] list-inside">
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/items/resources/variants"}>Variants</Link>
+                </Button>
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+
+        <Collapsible className="w-full">
+          <div className="flex justify-between items-center w-full">
+            <Button variant={"link"} className="text-xl px-0">
+              <Link to={"/items/consumables"}>Consumables</Link>
+            </Button>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <ChevronsUpDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="list-['-_'] list-inside">
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/items/consumables/variants"}>Variants</Link>
+                </Button>
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+
+        <Collapsible className="w-full">
+          <div className="flex justify-between items-center w-full">
+            <Button variant={"link"} className="text-xl px-0">
+              <Link to={"/items/weapons"}>Weapons</Link>
+            </Button>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <ChevronsUpDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="list-['-_'] list-inside">
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/items/weapons/variants"}>Variants</Link>
+                </Button>
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+
+        <Collapsible className="w-full">
+          <div className="flex justify-between items-center w-full">
+            <Button variant={"link"} className="text-xl px-0">
+              <Link to={"/items/armors"}>Armors</Link>
+            </Button>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <ChevronsUpDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="list-['-_'] list-inside">
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/items/armors/variants"}>Variants</Link>
+                </Button>
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+
+        <Button variant={"link"} className="text-xl pl-0">
+          <Link to={"/items/misc"}>Misc Items</Link>
+        </Button>
+
+        <Collapsible className="w-full">
+          <div className="flex justify-between items-center w-full">
+            <Button variant={"link"} className="text-xl px-0">
+              <Link to={"/monsters"}>Monsters</Link>
+            </Button>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <ChevronsUpDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="list-['-_'] list-inside">
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/monsters/variants"}>Variants</Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/monsters/drop-tables"}>Drop Tables</Link>
+                </Button>
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+
+        <Collapsible className="w-full">
+          <div className="flex justify-between items-center w-full">
+            <Button variant={"link"} className="text-xl px-0">
+              <Link to={"/npcs"}>NPC&apos;s</Link>
+            </Button>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <ChevronsUpDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="list-['-_'] list-inside">
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/npcs/vendors"}>Vendors</Link>
+                </Button>
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+
+        <Collapsible className="w-full">
+          <div className="flex justify-between items-center w-full">
+            <Button variant={"link"} className="text-xl px-0">
+              <Link to={"/quests"}>Quests</Link>
+            </Button>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <ChevronsUpDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="list-['-_'] list-inside">
+              <li>
+                <Button variant={"link"} className="text-base pl-0">
+                  <Link to={"/quests/steps"}>Steps</Link>
                 </Button>
               </li>
             </ul>
