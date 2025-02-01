d1aa2d7d8e51a22ed54539f6b4a3cb12f3060836
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-01 15:26:34 -0500

    Add content for gathering skills

--- src/features/skills/gathering/components/GatheringSkillDetailsContent.tsx
+++ src/features/skills/gathering/components/GatheringSkillDetailsContent.tsx
@@ -1,3 +1,33 @@
-export default function GatheringSkillDetailsContent() {
-  return <p>Gathering Skill Details Content</p>;
+import RecordLink from "@/components/recordLink/RecordLink";
+import ScrollList from "@/components/scrollList/ScrollList";
+import { components } from "@/types/api";
+
+export default function GatheringSkillDetailsContent({
+  record,
+}: {
+  record: components["schemas"]["GatheringSkillEntity"];
+}) {
+  return (
+    <>
+      <p className="text-xl">Id: {record.id}</p>
+      <div className="flex gap-3 items-center">
+        <p className="text-xl">Region: </p>
+        <RecordLink
+          recordBasePath="/regions"
+          recordId={record.region.id}
+          recordName={record.region.name}
+        />
+      </div>
+      <ScrollList
+        basePath="/items/resources"
+        title="Resources"
+        items={record.resources}
+      />
+      <ScrollList
+        basePath="/skills/gathering/requirements"
+        title="Requirements"
+        items={record.skillRequirements}
+      />
+    </>
+  );
 }
--- src/features/skills/gathering/components/GatheringSkillsIndexContent.tsx
+++ src/features/skills/gathering/components/GatheringSkillsIndexContent.tsx
@@ -1,3 +1,31 @@
-export default function GatheringSkillsIndexContent() {
-  return <p>Gathering Skills Index Content</p>;
+import RecordLink from "@/components/recordLink/RecordLink";
+import { components } from "@/types/api";
+
+export default function GatheringSkillsIndexContent({
+  data,
+  gridColsRule,
+}: {
+  data: components["schemas"]["GatheringSkillEntity"][];
+  gridColsRule: string;
+}) {
+  return data.map((skill) => (
+    <div
+      className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
+      key={skill.id}
+    >
+      <RecordLink
+        recordBasePath="/skills/gathering"
+        recordId={skill.id}
+        recordName={skill.name}
+      />
+      <RecordLink
+        recordBasePath="/regions"
+        recordId={skill.region.id}
+        recordName={skill.region.name}
+      />
+      <p>{skill.id}</p>
+      <p>{skill.resources.length}</p>
+      <p>{skill.skillRequirements.length}</p>
+    </div>
+  ));
 }
