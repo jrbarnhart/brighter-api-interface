35ce8ff5e7cc319576e741669dab292cd23abdad
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-01 14:44:03 -0500

    Add content for combat skill details

--- src/features/skills/combat/components/CombatSkillDetailsContent.tsx
+++ src/features/skills/combat/components/CombatSkillDetailsContent.tsx
@@ -1,3 +1,33 @@
-export default function CombatSkillDetailsContent() {
-  return <p>Combat Skill Details Content</p>;
+import RecordLink from "@/components/recordLink/RecordLink";
+import ScrollList from "@/components/scrollList/ScrollList";
+import { components } from "@/types/api";
+
+export default function CombatSkillDetailsContent({
+  combatSkill,
+}: {
+  combatSkill: components["schemas"]["CombatSkillEntity"];
+}) {
+  return (
+    <>
+      <p className="text-xl">Id: {combatSkill.id}</p>
+      <div className="flex gap-3 items-center">
+        <p className="text-xl">Region: </p>
+        <RecordLink
+          recordBasePath="/regions"
+          recordId={combatSkill.region.id}
+          recordName={combatSkill.region.name}
+        />
+      </div>
+      <ScrollList
+        basePath="/monsters"
+        title="Monsters"
+        items={combatSkill.monsters}
+      />
+      <ScrollList
+        basePath="/skills/combat/requirements"
+        title="Requirements"
+        items={combatSkill.skillRequirements}
+      />
+    </>
+  );
 }
