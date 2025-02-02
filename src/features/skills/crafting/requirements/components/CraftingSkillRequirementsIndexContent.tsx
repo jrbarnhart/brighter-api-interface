60ad659f962c2b69f1753bd8ef98d87fbae1fad6
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-02 18:24:34 -0500

    Split content into groups by skill id

--- src/features/skills/crafting/requirements/components/CraftingSkillRequirementsIndexContent.tsx
+++ src/features/skills/crafting/requirements/components/CraftingSkillRequirementsIndexContent.tsx
@@ -1,4 +1,10 @@
 import RecordLink from "@/components/recordLink/RecordLink";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { groupReqsBySkillId } from "@/lib/dataUtils";
 import { components } from "@/types/api";
 
 export default function CraftingSkillRequirementsIndexContent({
@@ -8,25 +14,40 @@ export default function CraftingSkillRequirementsIndexContent({
   data: components["schemas"]["CraftingSkillRequirementEntity"][];
   gridColsRule: string;
 }) {
-  return data.map((req) => (
-    <div
-      className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
-      key={req.id}
-    >
-      <RecordLink
-        recordBasePath="/skills/crafting/requirements"
-        recordId={req.id}
-        recordName={req.id.toString()}
-      />
-      <p>{req.unlockLevel}</p>
-      <p className="truncate">{req.description}</p>
-      {req.recipe && (
-        <RecordLink
-          recordBasePath="/skills/crafting/recipes"
-          recordId={req.recipe.id}
-          recordName={req.recipe.name}
-        />
-      )}
-    </div>
-  ));
+  const groupedData = groupReqsBySkillId(data);
+
+  return groupedData.map((group, index) => {
+    return (
+      <Collapsible key={index} defaultOpen={true}>
+        <CollapsibleTrigger asChild>
+          <p className="select-none p-2 text-lg bg-border">
+            {group[0]?.skill.name ?? "Group Name"}
+          </p>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          {group.map((req) => (
+            <div
+              className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
+              key={req.id}
+            >
+              <RecordLink
+                recordBasePath="/skills/crafting/requirements"
+                recordId={req.id}
+                recordName={req.id.toString()}
+              />
+              <p>{req.unlockLevel}</p>
+              <p className="truncate">{req.description}</p>
+              {req.recipe && (
+                <RecordLink
+                  recordBasePath="/skills/crafting/recipes"
+                  recordId={req.recipe.id}
+                  recordName={req.recipe.name}
+                />
+              )}
+            </div>
+          ))}
+        </CollapsibleContent>
+      </Collapsible>
+    );
+  });
 }
