d80682fccb0ef76c0667b94c789ca3992d49cfb8
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-02 15:11:28 -0500

    Add contents for combat reqs

--- src/features/skills/combat/requirements/components/CombatSkillRequirementDetailsContent.tsx
+++ src/features/skills/combat/requirements/components/CombatSkillRequirementDetailsContent.tsx
@@ -1,3 +1,33 @@
-export default function CombatSkillRequirementDetailsContent() {
-  return <p>Combat Skill Req Details</p>;
+import RecordLink from "@/components/recordLink/RecordLink";
+import { components } from "@/types/api";
+
+export default function CombatSkillRequirementDetailsContent({
+  record,
+}: {
+  record: components["schemas"]["CombatSkillRequirementEntity"];
+}) {
+  return (
+    <>
+      <p className="text-xl">Id: {record.id}</p>
+      <div className="flex gap-3 items-center">
+        <p className="text-xl">Skill: </p>
+        <RecordLink
+          recordBasePath="/skills/combat"
+          recordId={record.skillId}
+          recordName={record.skill.name}
+        />
+      </div>
+      {record.description && <p>Description: {record.description}</p>}
+      {record.monsterVariant && (
+        <div className="flex gap-3 items-center">
+          <p className="text-xl">Monster Variant: </p>
+          <RecordLink
+            recordBasePath="/monsterse/variants"
+            recordId={record.monsterVariant.id}
+            recordName={`${record.monsterVariant.name} ${record.monsterVariant.monster.name}`}
+          />
+        </div>
+      )}
+    </>
+  );
 }
--- src/features/skills/combat/requirements/components/CombatSkillRequirementsIndexContent.tsx
+++ src/features/skills/combat/requirements/components/CombatSkillRequirementsIndexContent.tsx
@@ -1,3 +1,32 @@
-export default function CombatSkillRequirementsIndexContent() {
-  return <p>Combat Skill Reqs Index</p>;
+import RecordLink from "@/components/recordLink/RecordLink";
+import { components } from "@/types/api";
+
+export default function CombatSkillRequirementsIndexContent({
+  data,
+  gridColsRule,
+}: {
+  data: components["schemas"]["CombatSkillRequirementEntity"][];
+  gridColsRule: string;
+}) {
+  return data.map((req) => (
+    <div
+      className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
+      key={req.id}
+    >
+      <RecordLink
+        recordBasePath="/skills/combat/requirements"
+        recordId={req.id}
+        recordName={`Lvl: ${req.unlockLevel.toString()}`}
+      />
+      <p>{req.id}</p>
+      <p className="truncate">{req.description}</p>
+      {req.monsterVariant && (
+        <RecordLink
+          recordBasePath="/monsters/variants"
+          recordId={req.monsterVariant.id}
+          recordName={`${req.monsterVariant.name} ${req.monsterVariant.monster.name}`}
+        />
+      )}
+    </div>
+  ));
 }
