8a6167d5305f57d53a2c5ba3b9793d40f01c09d6
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-02 15:17:12 -0500

    Add pages for combat reqs

--- src/features/skills/combat/requirements/pages/CombatSkillRequirementDetails.tsx
+++ src/features/skills/combat/requirements/pages/CombatSkillRequirementDetails.tsx
@@ -1,5 +1,17 @@
+import FeatureDetails from "@/components/featureDetails/FeatureDetails";
 import CombatSkillRequirementDetailsContent from "../components/CombatSkillRequirementDetailsContent";
+import UpdateCombatSkillRequirementForm from "../components/UpdateCombatSkillRequirementForm";
 
 export default function CombatSkillRequirementDetails() {
-  return <CombatSkillRequirementDetailsContent />;
+  return (
+    <FeatureDetails
+      redirectPath="/skills/combat/requirements"
+      url={`${import.meta.env.VITE_API_URL}/skills/combat/requirements`}
+      renderContentFn={CombatSkillRequirementDetailsContent}
+      updateForm={<UpdateCombatSkillRequirementForm />}
+      queryKeyName="combat-skill-requirements"
+      recordLabel="Combat Skill Requirement"
+      deleteNotes=""
+    />
+  );
 }
--- src/features/skills/combat/requirements/pages/CombatSkillRequirementsIndex.tsx
+++ src/features/skills/combat/requirements/pages/CombatSkillRequirementsIndex.tsx
@@ -1,5 +1,25 @@
+import FeatureIndex from "@/components/featureIndex/FeatureIndex";
+import useGetRecords from "@/queries/useGetAllRecords";
+
+import { paths } from "@/types/api";
+import { ComponentState } from "react";
 import CombatSkillRequirementsIndexContent from "../components/CombatSkillRequirementsIndexContent";
 
 export default function CombatSkillRequirementsIndex() {
-  return <CombatSkillRequirementsIndexContent />;
+  const combatSkillsUseQueryResult = useGetRecords<{
+    data: paths["/skills/combat/requirements"]["get"]["responses"]["200"]["content"]["application/json"];
+  }>({
+    queryKeyName: "combat-skill-requirements",
+    url: `${import.meta.env.VITE_API_URL}/skills/combat/requirements`,
+  });
+
+  return (
+    <FeatureIndex<ComponentState["schemas"]["CombatSkillRequirementEntity"]>
+      featureLabel="Combat Skill Requirements"
+      featureHeaders={["Unlock", "Id", "Description", "Monster Variant"]}
+      gridColsRule="grid-cols-[1fr_1fr_4fr_1fr]"
+      featureUseQueryResult={combatSkillsUseQueryResult}
+      renderContentFn={CombatSkillRequirementsIndexContent}
+    />
+  );
 }
