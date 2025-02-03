f749479e998d67279a673e9c219d3dbb3fa3fcdd
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-03 15:44:53 -0500

    Group recipes by skill

--- src/features/skills/crafting/recipes/components/RecipesIndexContent.tsx
+++ src/features/skills/crafting/recipes/components/RecipesIndexContent.tsx
@@ -1,4 +1,10 @@
 import RecordLink from "@/components/recordLink/RecordLink";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { groupRecipesBySkillId } from "@/lib/dataUtils";
 import { components } from "@/types/api";
 
 export default function RecipesIndexContent({
@@ -8,40 +14,60 @@ export default function RecipesIndexContent({
   data: components["schemas"]["CraftingRecipeEntity"][];
   gridColsRule: string;
 }) {
-  return data.map((recipe) => (
-    <div
-      className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
-      key={recipe.id}
-    >
-      <RecordLink
-        recordBasePath="/skills/crafting/recipes"
-        recordId={recipe.id}
-        recordName={recipe.name}
-      />
-      <p>{recipe.id}</p>
-      <p>{recipe.inputResourceVariants.length}</p>
-      <p>{recipe.inputItems.length}</p>
-      {recipe.outputConsumableVariant && (
-        <RecordLink
-          recordBasePath="items/consumables/variants"
-          recordId={recipe.outputConsumableVariant.id}
-          recordName={`${recipe.outputConsumableVariant.name} ${recipe.outputConsumableVariant.consumable.name}`}
-        />
-      )}
-      {recipe.outputArmorVariant && (
-        <RecordLink
-          recordBasePath="items/armors/variants"
-          recordId={recipe.outputArmorVariant.id}
-          recordName={`${recipe.outputArmorVariant.name} ${recipe.outputArmorVariant.armor.name}`}
-        />
-      )}
-      {recipe.outputWeaponVariant && (
-        <RecordLink
-          recordBasePath="items/weapons/variants"
-          recordId={recipe.outputWeaponVariant.id}
-          recordName={`${recipe.outputWeaponVariant.name} ${recipe.outputWeaponVariant.weapon.name}`}
-        />
-      )}
-    </div>
-  ));
+  const { grouped, noRequirement } = groupRecipesBySkillId(data);
+  const allRecipes = [...grouped, noRequirement];
+  return (
+    <>
+      {allRecipes.map((group, index) => {
+        if (group.length <= 0) return null;
+        return (
+          <Collapsible key={index} defaultOpen={true}>
+            <CollapsibleTrigger asChild>
+              <p className="select-none p-2 text-lg bg-border">
+                {group[0]?.requirement?.skillId ?? "Group Name"}
+              </p>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              {group.map((recipe) => (
+                <div
+                  className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
+                  key={recipe.id}
+                >
+                  <RecordLink
+                    recordBasePath="/skills/crafting/recipes"
+                    recordId={recipe.id}
+                    recordName={recipe.name}
+                  />
+                  <p>{recipe.id}</p>
+                  <p>{recipe.inputResourceVariants.length}</p>
+                  <p>{recipe.inputItems.length}</p>
+                  {recipe.outputConsumableVariant && (
+                    <RecordLink
+                      recordBasePath="items/consumables/variants"
+                      recordId={recipe.outputConsumableVariant.id}
+                      recordName={`${recipe.outputConsumableVariant.name} ${recipe.outputConsumableVariant.consumable.name}`}
+                    />
+                  )}
+                  {recipe.outputArmorVariant && (
+                    <RecordLink
+                      recordBasePath="items/armors/variants"
+                      recordId={recipe.outputArmorVariant.id}
+                      recordName={`${recipe.outputArmorVariant.name} ${recipe.outputArmorVariant.armor.name}`}
+                    />
+                  )}
+                  {recipe.outputWeaponVariant && (
+                    <RecordLink
+                      recordBasePath="items/weapons/variants"
+                      recordId={recipe.outputWeaponVariant.id}
+                      recordName={`${recipe.outputWeaponVariant.name} ${recipe.outputWeaponVariant.weapon.name}`}
+                    />
+                  )}
+                </div>
+              ))}
+            </CollapsibleContent>
+          </Collapsible>
+        );
+      })}
+    </>
+  );
 }
--- src/lib/dataUtils.ts
+++ src/lib/dataUtils.ts
@@ -9,3 +9,20 @@ export function groupReqsBySkillId<T extends { skillId: number }>(
     }, {})
   );
 }
+
+export function groupRecipesBySkillId<
+  T extends { requirement?: { skillId: number } }
+>(data: T[]): { grouped: T[][]; noRequirement: T[] } {
+  const grouped: { [key: number]: T[] } = {};
+  const noRequirement: T[] = [];
+
+  for (const item of data) {
+    if (item.requirement?.skillId !== undefined) {
+      (grouped[item.requirement.skillId] ??= []).push(item);
+    } else {
+      noRequirement.push(item);
+    }
+  }
+
+  return { grouped: Object.values(grouped), noRequirement };
+}
