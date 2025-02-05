8ed1170286cd1eced5249d16242cbaa982aae1d9
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-05 15:39:27 -0500

    Sort consumables by skill

--- src/features/consumables/components/ConsumablesIndexContent.tsx
+++ src/features/consumables/components/ConsumablesIndexContent.tsx
@@ -1,4 +1,10 @@
 import RecordLink from "@/components/recordLink/RecordLink";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { groupDataBySkillId } from "@/lib/dataUtils";
 import { components } from "@/types/api";
 
 export default function ConsumablesIndexContent({
@@ -8,20 +14,33 @@ export default function ConsumablesIndexContent({
   data: components["schemas"]["ConsumableEntity"][];
   gridColsRule: string;
 }) {
-  return data.map((consumable) => {
+  const groupedData = groupDataBySkillId(data);
+
+  return groupedData.map((group, index) => {
     return (
-      <div
-        className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
-        key={consumable.id}
-      >
-        <RecordLink
-          recordBasePath="/items/consumables"
-          recordId={consumable.id}
-          recordName={consumable.name}
-        />
-        <p>{consumable.id}</p>
-        <p>{consumable.variants.length}</p>
-      </div>
+      <Collapsible key={index} defaultOpen={true}>
+        <CollapsibleTrigger asChild>
+          <p className="select-none p-2 text-lg bg-border">
+            {group[0]?.skill?.name ?? "No Skill"}
+          </p>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          {group.map((consumable) => (
+            <div
+              className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
+              key={consumable.id}
+            >
+              <RecordLink
+                recordBasePath="/items/consumables"
+                recordId={consumable.id}
+                recordName={consumable.name}
+              />
+              <p>{consumable.id}</p>
+              <p>{consumable.variants.length}</p>
+            </div>
+          ))}
+        </CollapsibleContent>
+      </Collapsible>
     );
   });
 }
