a46a57335cbc48e0a9d75c7dc6721c47233602a1
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-08 20:11:01 -0500

    Add fn for testing if two arrays are equal

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function arraysAreEqual(arr1: number[], arr2: number[]): boolean {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
 }
