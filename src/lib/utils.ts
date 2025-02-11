487575a6b0c9b8c1c0f559535aaf46566571438c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-10 23:43:26 -0500

    Allow for arrays of strings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function arraysAreEqual(arr1: number[], arr2: number[]): boolean {
+export function arraysAreEqual(
+  arr1: number[] | string[],
+  arr2: number[] | string[]
+): boolean {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
