0d1316072b7df4045186bc05aaccd656074ecb09
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-19 18:44:47 -0500

    Add util for converting camel to title case

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,8 @@ export function arraysAreEqual(
   }
   return true;
 }
+
+export function camelToTitleCase(text: string): string {
+  const result = text.replace(/([A-Z])/g, " $1");
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
