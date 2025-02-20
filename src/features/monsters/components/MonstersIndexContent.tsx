6d36037c7d8b21f5b4f65d4ddf407f4bc35a059c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-19 20:06:48 -0500

    Order by region instead of skill

--- src/features/monsters/components/MonstersIndexContent.tsx
+++ src/features/monsters/components/MonstersIndexContent.tsx
@@ -4,7 +4,7 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { groupDataBySkillId } from "@/lib/dataUtils";
+import { groupDataByRegionId } from "@/lib/dataUtils";
 import { components } from "@/types/api";
 
 export default function MonstersIndexContent({
@@ -14,14 +14,14 @@ export default function MonstersIndexContent({
   data: components["schemas"]["MonsterEntity"][];
   gridColsRule: string;
 }) {
-  const groupedData = groupDataBySkillId(data);
+  const groupedData = groupDataByRegionId(data);
 
   return groupedData.map((group, index) => {
     return (
       <Collapsible key={index} defaultOpen={true}>
         <CollapsibleTrigger asChild>
           <p className="select-none p-2 text-lg bg-border">
-            {group[0]?.skill.name ?? "Group Name"}
+            {group[0]?.region.name ?? "Group Name"}
           </p>
         </CollapsibleTrigger>
         <CollapsibleContent>
