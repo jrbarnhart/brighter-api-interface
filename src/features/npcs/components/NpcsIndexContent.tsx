2588dbd4d49e78dae8af2a9dbb1d2eeeeed1373c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-06 16:38:46 -0500

    Map grouped data to collapsible

--- src/features/npcs/components/NpcsIndexContent.tsx
+++ src/features/npcs/components/NpcsIndexContent.tsx
@@ -1,4 +1,10 @@
 import RecordLink from "@/components/recordLink/RecordLink";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { groupDataByAlpha } from "@/lib/dataUtils";
 import { components } from "@/types/api";
 
 export default function NpcsIndexContent({
@@ -8,28 +14,41 @@ export default function NpcsIndexContent({
   data: components["schemas"]["NpcEntity"][];
   gridColsRule: string;
 }) {
-  return data.map((npc) => (
-    <div
-      className={`grid ${gridColsRule} gap-2 items-center p-2 even:bg-secondary/60`}
-      key={npc.id}
-    >
-      <RecordLink
-        recordBasePath="/npcs"
-        recordId={npc.id}
-        recordName={npc.name}
-      />
-      <p>{npc.id}</p>
-      {npc.vendor ? (
-        <RecordLink
-          recordBasePath="/npcs/vendors"
-          recordId={npc.vendor.id}
-          recordName={`Id: ${npc.vendor.id.toString()}`}
-        />
-      ) : (
-        <p>Not Set</p>
-      )}
-      <p>{npc.questSteps.length}</p>
-      <p>{npc.rooms.length}</p>
-    </div>
+  const groupedData = groupDataByAlpha(data);
+
+  return groupedData.map((group, index) => (
+    <Collapsible key={index} defaultOpen={true}>
+      <CollapsibleTrigger asChild>
+        <p className="select-none p-2 text-lg bg-border">
+          {group[0]?.name[0]?.toUpperCase() ?? "*"}
+        </p>
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        {group.map((npc) => (
+          <div
+            className={`grid ${gridColsRule} gap-2 items-center p-2 even:bg-secondary/60`}
+            key={npc.id}
+          >
+            <RecordLink
+              recordBasePath="/npcs"
+              recordId={npc.id}
+              recordName={npc.name}
+            />
+            <p>{npc.id}</p>
+            {npc.vendor ? (
+              <RecordLink
+                recordBasePath="/npcs/vendors"
+                recordId={npc.vendor.id}
+                recordName={`Id: ${npc.vendor.id.toString()}`}
+              />
+            ) : (
+              <p>Not Set</p>
+            )}
+            <p>{npc.questSteps.length}</p>
+            <p>{npc.rooms.length}</p>
+          </div>
+        ))}
+      </CollapsibleContent>
+    </Collapsible>
   ));
 }
