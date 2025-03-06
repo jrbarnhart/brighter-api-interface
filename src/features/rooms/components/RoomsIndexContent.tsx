4451537beb48670d562cdc85d91cd2a43b493367
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-06 18:17:25 -0500

    Group rooms by region

--- src/features/rooms/components/RoomsIndexContent.tsx
+++ src/features/rooms/components/RoomsIndexContent.tsx
@@ -1,4 +1,10 @@
 import RecordLink from "@/components/recordLink/RecordLink";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { groupDataByRegionId } from "@/lib/dataUtils";
 import { components } from "@/types/api";
 
 export default function RoomsIndexContent({
@@ -8,30 +14,44 @@ export default function RoomsIndexContent({
   data: components["schemas"]["RoomEntity"][];
   gridColsRule: string;
 }) {
-  return data.map((room) => (
-    <div
-      className={`grid ${gridColsRule} gap-2 items-center p-2 even:bg-secondary/60`}
-      key={room.id}
-    >
-      <RecordLink
-        recordBasePath="/rooms"
-        recordId={room.id}
-        recordName={room.name}
-      />
-      <p>{room.id}</p>
-      <RecordLink
-        recordBasePath="/regions"
-        recordId={room.region.id}
-        recordName={room.region.name}
-      />
-      <p>{room.portal && "✅"}</p>
-      <p>{room.obelisk && "✅"}</p>
-      <p>{room.banks.length}</p>
-      <p className="truncate">{room.craftingSkills.length}</p>
-      <p className="truncate">{room.monsters.length}</p>
-      <p className="truncate">{room.npcs.length}</p>
-      <p className="truncate">{room.resources.length}</p>
-      <p>{room.questSteps.length}</p>
-    </div>
+  const groupedData = groupDataByRegionId(data);
+
+  return groupedData.map((group, index) => (
+    <Collapsible key={index} defaultOpen={true}>
+      <CollapsibleTrigger asChild>
+        <p className="select-none p-2 text-lg bg-border">
+          {group[0]?.region.name ?? "Region Name"}
+        </p>
+      </CollapsibleTrigger>
+
+      <CollapsibleContent>
+        {group.map((room) => (
+          <div
+            className={`grid ${gridColsRule} gap-2 items-center p-2 even:bg-secondary/60`}
+            key={room.id}
+          >
+            <RecordLink
+              recordBasePath="/rooms"
+              recordId={room.id}
+              recordName={room.name}
+            />
+            <p>{room.id}</p>
+            <RecordLink
+              recordBasePath="/regions"
+              recordId={room.region.id}
+              recordName={room.region.name}
+            />
+            <p>{room.portal && "✅"}</p>
+            <p>{room.obelisk && "✅"}</p>
+            <p>{room.banks.length}</p>
+            <p className="truncate">{room.craftingSkills.length}</p>
+            <p className="truncate">{room.monsters.length}</p>
+            <p className="truncate">{room.npcs.length}</p>
+            <p className="truncate">{room.resources.length}</p>
+            <p>{room.questSteps.length}</p>
+          </div>
+        ))}
+      </CollapsibleContent>
+    </Collapsible>
   ));
 }
