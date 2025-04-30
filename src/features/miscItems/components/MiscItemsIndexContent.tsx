f0b45f6df22a925345797643da83399b3fdef224
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-30 15:40:02 -0400

    Sort by name

--- src/features/miscItems/components/MiscItemsIndexContent.tsx
+++ src/features/miscItems/components/MiscItemsIndexContent.tsx
@@ -8,22 +8,26 @@ export default function MiscItemsIndexContent({
   data: components["schemas"]["MiscItemEntity"][];
   gridColsRule: string;
 }) {
-  return data.map((miscItem) => {
-    return (
-      <div
-        className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
-        key={miscItem.id}
-      >
-        <RecordLink
-          recordBasePath="/items/misc"
-          recordId={miscItem.id}
-          recordName={miscItem.name}
-        />
-        <p>{miscItem.id}</p>
-        <p>{miscItem.inRecipes.length}</p>
-        <p>{miscItem.vendors.length}</p>
-        <p>{miscItem.dropTables.length}</p>
-      </div>
-    );
-  });
+  return data
+    .sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    })
+    .map((miscItem) => {
+      return (
+        <div
+          className={`grid ${gridColsRule} items-center p-2 even:bg-secondary/60`}
+          key={miscItem.id}
+        >
+          <RecordLink
+            recordBasePath="/items/misc"
+            recordId={miscItem.id}
+            recordName={miscItem.name}
+          />
+          <p>{miscItem.id}</p>
+          <p>{miscItem.inRecipes.length}</p>
+          <p>{miscItem.vendors.length}</p>
+          <p>{miscItem.dropTables.length}</p>
+        </div>
+      );
+    });
 }
